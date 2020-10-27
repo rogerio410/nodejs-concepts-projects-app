b3526bdd20ac9d9df4833ac7795009e108259a97
Rogerio Silva <rogerio410@gmail.com>
2020-10-27 08:27:07 -0300

    All endpoints OK. All Tests green

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.status(201).json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: `No repository for id "${id}"` })
+  }
+
+  likes = repositories[repoIndex].likes
+  repositories[repoIndex] = { id, title, url, likes, techs }
+
+  return response.status(200).json(repositories[repoIndex])
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: `No repository for id "${id}"` })
+  }
+
+  repositories.splice(repoIndex, 1)
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: `No repository for id "${id}"` })
+  }
+  repositories[repoIndex].likes++
+  return response.status(201).send(repositories[repoIndex])
 });
 
 module.exports = app;
